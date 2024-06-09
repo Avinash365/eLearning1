5a28f4cc33e8d0fda9266e898b2708394b868a7a
Avinash365 <akjhaharauli@gmail.com>
2024-06-09 11:16:29 +0530

    fixed auth issue

---
+++ frontend/src/pages/StudentPage.js
@@ -0,0 +1,14 @@
+
+import StudentDash from "../components/StudentDash.js";
+
+
+
+function StudentPage() {
+    return (
+        <div>
+            {/* <Navbar/>   */}
+            <StudentDash/>
+        </div>
+    )
+}; 
+export default StudentPage;
